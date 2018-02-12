6db83012da6290f96738029ffe3ea327b2bc9e30
Narazaka <info@narazaka.net>
2018-02-12 16:40:15 +0900

    先にイベント指定できるように

--- src/lib/middlewares/genericEventMiddleware.ts
+++ src/lib/middlewares/genericEventMiddleware.ts
@@ -1,7 +1,9 @@
 import { RequestContext, ShioriMiddlewareWithState } from "shiorack";
 import { handleRequestLazy, RequestCallbackReturnValue } from "shiori-request-helper";
 
-const middleware: <State>() => ShioriMiddlewareWithState<State, middleware.Events<State>> = () =>
+const middleware: <State>(events?: middleware.Events<State>) =>
+    ShioriMiddlewareWithState<State, middleware.EventsState<State>> =
+    <State>(events: middleware.Events<State> = {}) =>
     ({
         request: function genericEventMiddleware(ctx, next) {
             const id = ctx.request.headers.ID as string;
@@ -17,14 +19,20 @@ const middleware: <State>() => ShioriMiddlewareWithState<State, middleware.Event
                 return handleRequestLazy(ctx.request, next);
             }
         },
-        state: { events: {} },
+        state: { events },
     });
 
 // tslint:disable-next-line no-namespace
 namespace middleware {
-    export type SanaRequestCallback<State> = (ctx: RequestContext<State & Events<State>>) => RequestCallbackReturnValue;
+    export type SanaRequestCallback<State> =
+        (ctx: RequestContext<State & EventsState<State>>) => RequestCallbackReturnValue;
+
     export interface Events<State> {
-        events: {[name: string]: SanaRequestCallback<State>};
+        [name: string]: SanaRequestCallback<State>;
+    }
+
+    export interface EventsState<State> {
+        events: Events<State>;
     }
 }
 
