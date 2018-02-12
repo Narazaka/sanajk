aeb319438cceca37dec749e3f5ed16980babac35
Narazaka <info@narazaka.net>
2018-02-12 16:41:01 +0900

    useでSanaShioriBuilderを返す

--- src/lib/SanaShioriBuilder.ts
+++ src/lib/SanaShioriBuilder.ts
@@ -1,4 +1,11 @@
-import { ShioriBuilder } from "shiorack";
+import {
+    LoadMiddleware,
+    RequestMiddleware,
+    ShioriBuilder,
+    ShioriMiddleware,
+    ShioriMiddlewareWithState,
+    UnloadMiddleware,
+} from "shiorack";
 import * as completeResponseMiddleware from "../lib/middlewares/completeResponseMiddleware";
 import * as dirpathMiddleware from "../lib/middlewares/dirpathMiddleware";
 import * as errorResponseMiddleware from "../lib/middlewares/errorResponseMiddleware";
@@ -18,5 +25,61 @@ export class SanaShioriBuilder<State = {}> extends ShioriBuilder<State> {
             .use(senderMiddleware)
             .use(genericEventMiddleware())
             ;
+    }
+
+    /**
+     * add middleware
+     * @param middleware middleware with state
+     */
+    use(middleware: ShioriMiddleware<State>): SanaShioriBuilder<State>;
+    use<AddState>(middleware: ShioriMiddlewareWithState<State, AddState>): SanaShioriBuilder<State & AddState>;
+    use<AddState = {}>(middleware: ShioriMiddlewareWithState<State, AddState>) {
+        const builder = super.use(middleware);
+
+        return new SanaShioriBuilder(
+            builder.state,
+            {
+                load: builder.load,
+                request: builder.request,
+                unload: builder.unload,
+            },
+            false,
+        );
+    }
+
+    /**
+     * add load middleware
+     * @param middleware middleware
+     * @param state state
+     */
+    useLoad(middleware: LoadMiddleware<State>): SanaShioriBuilder<State>;
+    useLoad<AddState>(middleware: LoadMiddleware<State & AddState>, state: AddState):
+        SanaShioriBuilder<State & AddState>;
+    useLoad<AddState = {}>(middleware: LoadMiddleware<State & AddState>, state?: AddState) {
+        return super.useLoad(middleware, state);
+    }
+
+    /**
+     * add request middleware
+     * @param middleware middleware
+     * @param state state
+     */
+    useRequest(middleware: RequestMiddleware<State>): SanaShioriBuilder<State>;
+    useRequest<AddState>(middleware: RequestMiddleware<State & AddState>, state: AddState):
+        SanaShioriBuilder<State & AddState>;
+    useRequest<AddState = {}>(middleware: RequestMiddleware<State & AddState>, state?: AddState) {
+        return super.useRequest(middleware, state);
+    }
+
+    /**
+     * add unload middleware
+     * @param middleware middleware
+     * @param state state
+     */
+    useUnload(middleware: UnloadMiddleware<State>): SanaShioriBuilder<State>;
+    useUnload<AddState>(middleware: UnloadMiddleware<State & AddState>, state: AddState):
+        SanaShioriBuilder<State & AddState>;
+    useUnload<AddState = {}>(middleware: UnloadMiddleware<State & AddState>, state?: AddState) {
+        return super.useUnload(middleware, state);
     }
 }
