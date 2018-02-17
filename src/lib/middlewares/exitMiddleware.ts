390d3dbdbd85bab050076219fab31842d8a3cc5a
Narazaka <info@narazaka.net>
2018-02-17 12:36:14 +0900

    voidだった

--- src/lib/middlewares/exitMiddleware.ts
+++ src/lib/middlewares/exitMiddleware.ts
@@ -2,8 +2,8 @@ import * as shiorack from "shiorack";
 
 const middleware: shiorack.ShioriMiddleware<{}> = {
     unload: async function exitMiddleware(_ctx, next) {
-        const result = await next();
-        process.exit(result ? 0 : 1);
+        await next();
+        process.exit();
     },
 };
 
