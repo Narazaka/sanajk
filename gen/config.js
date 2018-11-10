871e309f08d248e11f741ba8db187be9f85019bb
Narazaka <info@narazaka.net>
2018-11-10 20:41:29 +0900

    no gen

--- gen/config.js
+++
@@ -1,9 +0,0 @@
-const { SanaShioriBuilder } = require("sanajk");
-
-const builder = new SanaShioriBuilder().useDefaults();
-
-const events = builder.state.events;
-const s = String.raw;
-events.OnBoot = () => s`\0\s[0]\e`;
-
-module.exports = builder.build();
--- gen/config.ts
+++
@@ -1,9 +0,0 @@
-import { SanaShioriBuilder } from "sanajk";
-
-const builder = new SanaShioriBuilder().useDefaults();
-
-const events = builder.state.events;
-const s = String.raw;
-events.OnBoot = () => s`\0\s[0]\e`;
-
-export = builder.build();
